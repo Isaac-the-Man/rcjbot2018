406bb290d063baf7fdebeec381200a3bcd96eb16
Isaac-the-Man <steven97102@gmail.com>
2018-07-26 10:08:03 +0800

    filter_tool updated, now support hsv and grey scale thresholding in both mode ! (video stream and image mode)

--- filter_tool.py
+++ filter_tool.py
@@ -1,7 +1,9 @@
 # This tool is for previewing an image or stream video with some of the basic filters
 # you have to specify the source of the image if image mode is chosen
 # the default mode is webcam aka video stream
-# for now it supports the following filter: grey_scale thresholding,
+# for now it supports the following filter: grey_scale thresholding, and HSV
+# example format for video: python filter_tool.py -f hsv
+# example format for image: python filter_tool.py -f hsv -i D:/home/img.jpg
 
 import cv2
 import argparse
@@ -10,12 +12,16 @@ import argparse
 def callback(val):       # this null function has no actual use
     pass
 
-def setup_trackbars():
+def setup_trackbars(filter):        # take in filter to decide the format of the trackbar
     cv2.namedWindow('Trackbars', cv2.WINDOW_AUTOSIZE)       # make sure that the window size is normal
 
     for i in ['Min', 'Max']:
         v = 0 if i == 'Min' else 255
-        cv2.createTrackbar('%s' %i, 'Trackbars', v, 255, callback)      # create the tracbars
+        if filter == 'THRESHOLD':
+            cv2.createTrackbar('%s' %i, 'Trackbars', v, 255, callback)      # create the tracbars for thresholding
+        elif filter == 'HSV':
+            for j in str(filter):
+                cv2.createTrackbar('%s_%s' %(i, j), 'Trackbars', v, 255, callback)      # trackbar for hsv
 
 def get_arguments():
     ap = argparse.ArgumentParser()
@@ -26,12 +32,17 @@ def get_arguments():
 
     return args
 
-def get_trackbar_values():      # return the trackbar values
+def get_trackbar_values(filter):      # return the trackbar values
     values = []
 
     for i in ['Min', 'Max']:
-        v = cv2.getTrackbarPos('%s' %i, 'Trackbars')
-        values.append(v)
+        if filter == 'THRESHOLD':
+            v = cv2.getTrackbarPos('%s' %i, 'Trackbars')
+            values.append(v)
+        elif filter == 'HSV':
+            for j in str(filter):
+                v = cv2.getTrackbarPos('%s_%s' %(i, j), 'Trackbars')
+                values.append(v)
 
     return values
 
@@ -48,18 +59,22 @@ def main():
     else:
         vid = cv2.VideoCapture(0)     # if no img path is given, stream vido mode will be on
 
-    setup_trackbars()       # initialize the trackbar
+    setup_trackbars(args['filter'].upper())       # initialize the trackbar
 
     while True:
-        if video_mode:
+        if video_mode:      # read the frame of the video stream
             _, frame = vid.read()
 
-        min, max = get_trackbar_values()        # update the value of the trackbar\
+        value = get_trackbar_values(args['filter'].upper())        # update the value of the trackbar
 
-        if args['filter'] == 'threshold':
+        if args['filter'].upper() == 'THRESHOLD':       # threhsolding selected
             frame2 = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
-            _, output = cv2.threshold(frame2.copy(), min, max, cv2.THRESH_BINARY_INV)        # threshold the frame or image
+            _, output = cv2.threshold(frame2.copy(), value[0], value[1], cv2.THRESH_BINARY_INV)        # threshold the frame or image
+        elif args['filter'].upper() == 'HSV':       # hsv filter chosen
+            frame2 = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2HSV)      # cvt the color to hsv
+            output = cv2.inRange(frame2, (value[0], value[1], value[2]), (value[3], value[4], value[5]))        # mask out the hsv color range
 
+        cv2.namedWindow('output', cv2.WINDOW_AUTOSIZE)      # create the window
         cv2.imshow('output', output)        # show the output image
 
         if cv2.waitKey(1) & 0xFF is ord('q'):       # waiting for the user to quit
